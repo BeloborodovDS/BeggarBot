9353f6f0d4b8bb0d8b3b3ba59c5db696a48cd9c8
BeloborodovDS <dmitribeloborodov@yandex.ru>
2017-06-23 12:12:55 +0000

    +frown control

--- test.cpp
+++ test.cpp
@@ -22,13 +22,17 @@ using namespace std;
 #define BB_DO_FLIP		1
 
 //pca9685 defines
-#define PCA_PIN_BASE 300
-#define PCA_MAX_PWM 4096
-#define PCA_HERTZ 50
+#define PCA_PIN_BASE 		300
+#define PCA_MAX_PWM 		4096
+#define PCA_HERTZ 		50
 
 //calibrated
-#define SERVO_MS_MIN 0.6
-#define SERVO_MS_MAX 2.4
+#define SERVO_MS_MIN 		0.6
+#define SERVO_MS_MAX 		2.4
+
+//PINS
+#define PIN_EYEBROW 		PCA_PIN_BASE  //eyebrow servo at pin location 0 on PCA controller
+#define PIN_PCA			PCA_PIN_BASE + 16 //all pins on PCA controller 
 
 //drive servo PIN at angle ANGLE for SG90 servo: 1ms=>0deg, 1.5ms=>90deg, 2ms=>180deg
 //angle: [0,180], pin: PIN_BASE(controller)+SERVO_NUM(0-15) or PIN_BASE(controller)+16 for all servos
@@ -45,6 +49,21 @@ void driveMs(float Ms, int pin)
   float cycleMs = 1000.0f / PCA_HERTZ;
   int ticks = (int)(PCA_MAX_PWM * Ms / cycleMs + 0.5f);
   pwmWrite(pin, ticks);
+}
+
+//move eyebrows: 
+// 0 for neutral expression
+// >0 for kind expression
+// <0 for angry expression
+void frown(int mode)
+{
+  if(mode==0)
+    driveDegs(60, PIN_EYEBROW);
+  else if (mode>0)
+    driveDegs(25, PIN_EYEBROW);
+  else
+    driveDegs(100, PIN_EYEBROW);
+  delay(1000);
 }
 
 int main( int argc, char** argv )
@@ -112,32 +131,26 @@ int main( int argc, char** argv )
   
     // Setup with pinbase 300 and i2c location 0x40 (default for pca9685)
     // PWM period for SG90 servos is 20ms (50Hz)
-    int fd = pca9685Setup(PCA_PIN_BASE, 0x40, PCA_HERTZ);
-    if (fd < 0)
+    int pca_fd = pca9685Setup(PCA_PIN_BASE, 0x40, PCA_HERTZ);
+    if (pca_fd < 0)
     {
 	    cout<<"Error in init PCA9685!"<<endl;
-	    return fd;
+	    return pca_fd;
     }
     // Reset all output
-    pca9685PWMReset(fd);
+    pca9685PWMReset(pca_fd);
     
-    //drive all servos to 5 deg
-    driveDegs(90, PCA_PIN_BASE + 16);
-    delay(1000);
-
-    while(true)
-    {
-      for (int i=0; i<4; i++)
-      {
-	driveDegs(175, PCA_PIN_BASE + i);
-	delay(1000);
-      }
-      for (int i=0; i<4; i++)
-      {
-	driveDegs(5, PCA_PIN_BASE + i);
-	delay(1000);
-      }
-    }
+    //drive all servos to 90 deg
+    driveDegs(90, PIN_PCA);
+    delay(2000);
+    
+    frown(0);
+    frown(1);
+    frown(-1);
+    frown(0);
   
+    //reset all servos before exit
+    driveDegs(90, PIN_PCA);
+    
     return 0;
 }
